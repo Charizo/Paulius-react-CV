b67aa565b4e069b9fed5cc09ba16bb0c59f248bd
Charizo <pauliusvolskis@gmail.com>
2023-11-14 21:04:32 +0000

    test commit 1 term

--- my-first-vite-project/components/Main.jsx
+++ my-first-vite-project/components/Main.jsx
@@ -169,7 +169,7 @@ export default function Main() {
       <header className="main-header">
         <h1 className="name">
           <span className="eros-name">Paulius</span>
-          <strong> Volskis</strong>
+          <strong> Volskisssss</strong>
         </h1>
         <h4 className="header-desc">
           <span className="eros-name">
